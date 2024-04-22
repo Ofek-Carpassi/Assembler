07a185eae9802070b72fc0ec3829335dd82ce7cb
OfekC <ofek.carpassi@gmail.com>
2024-04-22 13:42:26 +0300

    ARE

--- Source/secondPass.c
+++ Source/secondPass.c
@@ -358,10 +358,7 @@ void executeSecondPass(char *srcFile, char *tmpFileName, Node *symbolTableHead) 
                     binaryLine = (char *)calloc(15, sizeof(char));
                     binaryNumber = intToBinary(current->line, 12);
                     strcat(binaryLine, binaryNumber);
-                    if(strcmp(current->data, "external") == 0)
-                        strcat(binaryLine, "01");
-                    else
-                        strcat(binaryLine, "10");
+                    strcat*(binaryLine, "00");
                     base4 = convertToEncryptedBase4(binaryLine);
                     if(objLineNumber == 1)
                         fprintf(obj, "%s", base4);
